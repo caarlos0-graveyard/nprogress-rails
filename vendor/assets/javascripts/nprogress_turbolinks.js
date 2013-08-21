2dabcdb93c152bf003c82990eb18c6acad881880
Carlos Alexandro Becker <caarlos0@gmail.com>
2013-08-21 08:38:51 -0300

    Updating nprogress_turbolinks adding treatment for browser back button
    refs to rstacruz/nprogress#8

--- vendor/assets/javascripts/nprogress_turbolinks.js
+++ vendor/assets/javascripts/nprogress_turbolinks.js
@@ -1,4 +1,5 @@
 $(function() {
   $(document).on('page:fetch', function() { NProgress.start(); });
   $(document).on('page:load',  function() { NProgress.done(); });
+  $(document).on('page:restore', function() { NProgress.remove(); })
 });
