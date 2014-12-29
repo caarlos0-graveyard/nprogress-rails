852d4b8cfb780ac18b9203c7abaaa7ad8593df47
Luke Rollans <hello@lukerollans.me>
2014-12-29 15:32:59 +0800

    Bump NProgress to 70% when request has returned

--- app/assets/javascripts/nprogress-turbolinks.js
+++ app/assets/javascripts/nprogress-turbolinks.js
@@ -1,5 +1,6 @@
 $(function() {
   $(document).on('page:fetch',   function() { NProgress.start();  });
+  $(document).on('page:receive', function() { NProgress.set(0.7); })
   $(document).on('page:change',  function() { NProgress.done();   });
   $(document).on('page:restore', function() { NProgress.remove(); });
 });
