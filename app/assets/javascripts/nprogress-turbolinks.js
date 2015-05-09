b9771e0d354e8d16bb741ca9c196123207e87b74
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-05-09 09:27:07 -0300

    changed nprogress-turbolinks too

--- app/assets/javascripts/nprogress-turbolinks.js
+++ app/assets/javascripts/nprogress-turbolinks.js
@@ -1,6 +1,6 @@
-$(function() {
-  $(document).on('page:fetch',   function() { NProgress.start();  });
-  $(document).on('page:receive', function() { NProgress.set(0.7); });
-  $(document).on('page:change',  function() { NProgress.done();   });
-  $(document).on('page:restore', function() { NProgress.remove(); });
+jQuery(function() {
+  jQuery(document).on('page:fetch',   function() { NProgress.start();  });
+  jQuery(document).on('page:receive', function() { NProgress.set(0.7); });
+  jQuery(document).on('page:change',  function() { NProgress.done();   });
+  jQuery(document).on('page:restore', function() { NProgress.remove(); });
 });
